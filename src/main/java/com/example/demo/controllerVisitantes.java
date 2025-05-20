1d3dd5f33e8f1ec8eb8c3b63983784d03397dfd6
Richard Salamanca <richardgithub234@gmail.com>
2025-05-19 21:57:35 -0500

    enviar datos nuevos

--- src/main/java/com/example/demo/controllerVisitantes.java
+++ src/main/java/com/example/demo/controllerVisitantes.java
@@ -1,23 +1,32 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controllerVisitantes {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @PostMapping("/DatosVisitantes")
     public ResponseEntity<String> guardarDatos(@RequestBody VisitanteRequestTable visitante){
+String nombrePuesto = visitante.getNombrePuesto();
 
-     VisitanteRequestTable visitanteNuevo = new VisitanteRequestTable();
-     visitanteNuevo.setNombreApellidos(visitante.getNombreApellidos());
-     visitanteNuevo.setCedula(visitante.getCedula());
-     visitanteNuevo.setEmpresaPermiso(visitante.getEmpresaPermiso());
-     visitanteNuevo.setNombrePuesto(visitante.getNombrePuesto());
+String crearTabla = "CREATE TABLE IF NOT EXISTS" + nombrePuesto + "(" +
+        "id INT AUTO_INCREMENT PRIMARY KEY, " +
+        "nombre_apellido VARCHAR(100), "+
+        "cedula INT(15)" +
+        "puesto_ingreso VARCHAR(50)"+
+        "empresa_permiso VARCHAR(50)" +
 
-        return ResponseEntity.ok("datos enviados correctamente");
+")";
+        jdbcTemplate.execute(crearTabla);
+        return ResponseEntity.ok("datos enviado");
     }
 
 }
